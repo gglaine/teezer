2dee07a637806b631eb02d9ca4d1575613acda36
guillaumelaine <null>
2023-11-10 19:09:45 +0100

    filterbox

--- src/components/ProductsPage.vue
+++ src/components/ProductsPage.vue
@@ -1,10 +1,38 @@
 <template>
     <div class="container mx-auto px-4 mt-8 ">
-    <button @click="toggleSortOrder" class="w-full border-1 mt-2 mb-2 text-right border-white text-white p-2 rounded bg-transparent">
-        Sort Price {{ sortOrder === 'ASC' ? '🔽' : '🔼' }}
-    </button>
+			<div v-if="randomFunFact && showFunFact" class="random-fun-fact bg-slate-900 border-l-4 border-blue-500 text-blue-700 p-4 relative">
+				<p class="font-light mb-2">Did You Know?</p>
+				<div class="flex justify-between items-start">
+					<div>
+						<p class="font-bold mb-2 mt-2 text-white">{{ randomFunFact.title }}</p>
+						<p class="font-light text-white text-2xl">{{ randomFunFact.fact }}</p>
+					</div>
+					<p class=" text-white text-4xl absolute top-4 right-4">{{ randomFunFact.emoji }}</p>
+				</div>
+			</div>
+
+      <!-- Flex container for tags and sort button -->
+			<div class="flex justify-end items-center mb-4 mt-2">
+            <!-- Tags categories -->
+            <div class="flex gap-2">
+                <!-- Replace these divs with actual tag components or buttons -->
+								<div v-for="(tag, index) in uniqueTags" :key="index"
+								@click="filterByTag(tag)"
+								class="border-b border-slate-400 text-white text-sm p-1 cursor-pointer font-light"
+								:class="{ 'selected-tag': tag === selectedTag }">
+								{{ tag }}
+						</div>
+                <!-- ... add more categories as needed -->
+            </div>
+
+            <!-- Sort Button -->
+            <button @click="toggleSortOrder" class="border-1 border-white text-white text-sm p-2 font-light rounded bg-transparent">
+                Value {{ sortOrder === 'ASC' ? '🔽' : '🔼' }}
+            </button>
+        </div>
       <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4">
-        <div v-for="(product, index) in products" :key="product.id" @click="openLightbox(product)" class="group cursor-pointer mb-4 break-inside-avoid-column relative max-w-sm rounded overflow-hidden shadow-lg">
+
+        <div v-for="(product, index) in filteredProducts" :key="product.id" @click="openLightbox(product)" class="group cursor-pointer mb-4 break-inside-avoid-column relative max-w-sm rounded overflow-hidden shadow-lg">
             <div class="border border-transparent hover:border-blue-500 transition duration-300 w-full h-full absolute inset-0 rounded group"></div>
             <v-lazy-image
             :src="getProductImageUrl(product.localImage)"
@@ -12,11 +40,11 @@
             class="w-full rounded transform transition duration-300 group-hover:scale-110 relative z-10"
             aspect-ratio="1"
             />
-            <button @click.stop="toggleCaption(product.id, $event)" class="absolute bottom-4 right-4 bg-slate-700 z-50 w-10 text-white p-2 rounded-full">+</button>
+            <button @click.stop="toggleCaption(product.id, $event)" class="absolute bottom-2 right-2 bg-transparent z-50 w-10 text-white p-2 rounded-full">+</button>
             <div  v-if="visibleCaptions[product.id]" class="px-2 py-4 mt-2 text-left">
                 <p class="text-slate-500 text-base">{{ product.figcaption }}</p>
                 <!-- Display the price -->
-                <p class="text-gray-700 text-base">Price: {{ product.price }}</p>
+                <p class="text-gray-700 text-base mt-3">Sold: ${{ product.price }}</p>
             </div>
         </div>
             <!-- Lightbox Overlay -->
@@ -40,6 +68,9 @@
   </template>
   
   <style scoped>
+	.selected-tag {
+  background-color: #424547; /* Example blue color */
+}
   .break-inside-avoid-column {
     break-inside: avoid-column;
   }
@@ -67,6 +98,19 @@
 .group:hover .bg-random-color::before {
   transform: scale(1.05);
 }
+
+.random-fun-fact {
+  animation: fadeOut 15s forwards;
+}
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
   </style>
   
   <script>
@@ -77,16 +121,29 @@
     components: {
       VLazyImage,
     },
+		computed: {
+    uniqueTags() {
+        const allTags = this.products.flatMap(product => product.tags);
+        return ['All', ...new Set(allTags)];
+    }
+},
     data() {
       return {
         products: [],
         activeProduct: null, // Will hold the active product details for the lightbox
         visibleCaptions: {}, // Object to keep track of visible captions
-        sortOrder: 'DESC', // Track the sort order, default is 'ASC'
+        sortOrder: 'DESC', // Track the sort order, default is 'ASC',
+				randomFunFact: null,
+				showFunFact: true, // new property to control visibility
+				filteredProducts: [],
       };
     },
     mounted() {
         this.fetchProducts();
+				this.fetchFunFacts();
+				setTimeout(() => {
+      this.showFunFact = false;
+    }, 5000);
         window.scrollTo(0, 0); // Ensure the window scrolls to the top when the component is mounted
         const cards = document.querySelectorAll('.bg-random-color');
         cards.forEach(card => {
@@ -110,28 +167,63 @@
         getProductImageUrl(imageFileName) {
             return `/downloaded_images/${imageFileName}`;
         },
-        async fetchProducts() {
-            try {
-            const response = await fetch('/image-mappings.json');
-            if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
-            }
-            const productsData = await response.json();
-            this.products = productsData.map((product, index) => {
-                // Extract price using the regular expression
-                const priceMatch = product.figcaption.match(/\$\d+(?:,\d{3})*(?:\.\d{2})?/);
-                return {
-                ...product,
-                id: index, // Assuming index as a unique identifier for each product
-                figcaption: product.figcaption.split('    Completed')[0].trim(),
-                price: priceMatch ? parseFloat(priceMatch[0].replace(/[$,]/g, '')) : 0 // Convert price to number
-                };
-            });
-            this.sortProducts(); // Initial sort
-            } catch (error) {
-            console.error('There has been a problem with your fetch operation:', error);
-            }
-        },
+				async fetchProducts() {
+				try {
+						const response = await fetch('/image-mappings.json');
+						if (!response.ok) {
+								throw new Error('Network response was not ok: ' + response.statusText);
+						}
+						const productsData = await response.json();
+						this.products = productsData.map((product, index) => {
+								// Extract the part of the figcaption before "Completed"
+								const figcaptionMatch = product.figcaption.match(/^(.*?)\s+Completed/);
+								const cleanedFigcaption = figcaptionMatch ? figcaptionMatch[1].trim() : product.figcaption;
+
+								// Extract price using the regular expression
+								const priceMatch = product.figcaption.match(/\$\d+(?:,\d{3})*(?:\.\d{2})?/);
+								const price = priceMatch ? parseFloat(priceMatch[0].replace(/[$,]/g, '')) : 0; // Convert price to number
+							// Dynamically extract tags
+							const tags = this.extractTags(product.figcaption);
+								return {
+										...product,
+										id: index, // Assuming index as a unique identifier for each product
+										figcaption: cleanedFigcaption,
+										price: price,
+										tags: tags
+								};
+						});
+						this.sortProducts(); // Initial sort
+						this.filteredProducts = this.products;
+						console.log(this.products)
+					} catch (error) {
+							console.error('There has been a problem with your fetch operation:', error);
+					}
+				},
+			extractTags(figcaption) {
+					const keywords = ["Vintage", "Disney", "Rare", "No Feet", "Automat"]; // Add more as needed
+					return keywords.filter(keyword => figcaption.includes(keyword));
+			},
+			filterByTag(tag) {
+				this.selectedTag = tag;
+				if (tag === 'All') {
+					this.filteredProducts = this.products;
+				} else {
+					this.filteredProducts = this.products.filter(product =>
+						product.tags.includes(tag));
+				}
+			},
+			  async fetchFunFacts() {
+    try {
+      const response = await fetch('/pezfacts.json');
+      if (!response.ok) {
+        throw new Error('Network response was not ok: ' + response.statusText);
+      }
+      const funFacts = await response.json();
+      this.randomFunFact = funFacts[Math.floor(Math.random() * funFacts.length)];
+    } catch (error) {
+      console.error('Error fetching fun facts:', error);
+    }
+  },
       toggleSortOrder() {
         this.sortOrder = this.sortOrder === 'ASC' ? 'DESC' : 'ASC';
         this.sortProducts();
